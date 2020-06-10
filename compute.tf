90cbd9937cfc0442a2281456de7848d2266ea366
Malar Kandasamy <k.malarvizhi@in.ibm.com>
2020-06-10 18:19:12 +0530

    Removing security group, rule

--- compute.tf
+++ compute.tf
@@ -22,30 +22,6 @@ data "ibm_is_instance_profile" "vnf_profile" {
 # Create RHEL 7 virtual server.
 ##############################################################################
 
-/*
-resource "ibm_is_security_group" "rhel7_security_group" {
-    name = "secgroup"
-    vpc = "${data.ibm_is_vpc.rhel7_vpc.id}"
-}
-
-resource "ibm_is_security_group_rule" "test_rhel7_sg_allow_ssh" {
-  depends_on = [ibm_is_security_group.rhel7_security_group]
-  group     = ibm_is_security_group.rhel7_security_group.id
-  direction = "inbound"
-  remote     = "0.0.0.0/0"
-  tcp {
-    port_min = 22
-    port_max = 22
-  }
-}
-
-resource "ibm_is_security_group_rule" "test_rhel7_sg_rule_all" {
-  depends_on = [ibm_is_security_group_rule.test_rhel7_sg_allow_ssh]
-  group     = ibm_is_security_group.rhel7_security_group.id
-  direction = "inbound"
-  remote    = "0.0.0.0/0"
-}
-*/
 
 data "template_file" "user_data" {
   template = "${file("${path.module}/boot.tpl")}"
@@ -55,7 +31,6 @@ data "template_file" "user_data" {
 }
 
 resource "ibm_is_instance" "rhel7_vsi" {
-  // depends_on = [ibm_is_security_group_rule.test_rhel7_sg_rule_all]
   name           = "${var.vnf_instance_name}"
   image          = "${data.ibm_is_image.rhel7_custom_image.id}"
   profile        = "${data.ibm_is_instance_profile.vnf_profile.name}"
@@ -63,7 +38,6 @@ resource "ibm_is_instance" "rhel7_vsi" {
 
   primary_network_interface {
     subnet = "${data.ibm_is_subnet.rhel7_subnet1.id}"
-    // security_groups = [ibm_is_security_group.rhel7_security_group.id]
   }
 
   vpc  = "${data.ibm_is_vpc.rhel7_vpc.id}"
