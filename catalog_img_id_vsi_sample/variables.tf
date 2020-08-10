225b9d04192f211067f119d4eb4ebe836c63afb5
Malar Kandasamy <k.malarvizhi@in.ibm.com>
2020-08-10 12:31:08 +0530

    Adding files for catalog image id

---
+++ catalog_img_id_vsi_sample/variables.tf
@@ -0,0 +1,96 @@
+##############################################################################
+# Variable block - See each variable description
+##############################################################################
+
+variable "api_key" {
+  default     = ""
+}
+
+##############################################################################
+# zone - VPC zone where resources are to be provisioned.
+##############################################################################
+variable "zone" {
+  default     = "us-south-3"
+  description = "The VPC Zone that you want your VPC networks and virtual servers to be provisioned in. To list available zones, run `ibmcloud is zones`."
+}
+
+##############################################################################
+# vpc_name - VPC where resources are to be provisioned.
+##############################################################################
+variable "vpc_name" {
+  default     = ""
+  description = "The name of your VPC where Ubuntu VSI is to be provisioned."
+}
+
+##############################################################################
+# subnet_id - Subnet where resources are to be provisioned.
+##############################################################################
+variable "subnet_id"{
+  default = ""
+  description =" The id of the subnet to which Ubuntu VSI's interface belongs to"
+}
+
+
+##############################################################################
+# ssh_key_name - The name of the public SSH key to be used when provisining Ubuntu VSI.
+##############################################################################
+variable "ssh_key_name" {
+  default     = ""
+  description = "The name of the public SSH key to be used when provisining Ubuntu VSI."
+}
+
+##############################################################################
+# vnf_vpc_image_id - The id of the catalog image to be provisioned in your IBM Cloud account.
+##############################################################################
+
+variable "vnf_vpc_image_id" {
+  default = ""
+  description = "The id of the catalog image to be provisioned in your IBM Cloud account."
+}
+
+##############################################################################
+# vnf_vpc_instance_name - The name of your Ubuntu Virtual Server to be provisioned
+##############################################################################
+variable "vnf_instance_name" {
+  default     = "ubuntu18-04-vsi"
+  description = "The name of your Ubuntu Virtual Server to be provisioned."
+}
+
+##############################################################################
+# vnf_profile - The profile of compute CPU and memory resources to be used when provisioning Ubuntu VSI.
+##############################################################################
+variable "vnf_profile" {
+  default     = "bx2-2x8"
+  description = "The profile of compute CPU and memory resources to be used when provisioning Ubuntu VSI. To list available profiles, run `ibmcloud is instance-profiles`."
+}
+
+variable "region" {
+  default     = "us-south"
+  description = "Optional. The value of the region of VPC."
+}
+
+##############################################################################
+# resource_group - The resource group to which the VPC belongs to.
+##############################################################################
+variable "resource_group" {
+  default     = "Default"
+  description = "Optional. The value of the resource group of VPC."
+}
+
+#####################################################################################################
+# api_key - This is the ibm_cloud_api_key which should be used only while testing this code from CLI. 
+# It is not needed while testing from Schematics
+######################################################################################################
+/*
+variable "api_key" {
+  default     = ""
+  description = "holds the user api key"
+}*/
+
+##############################################################################
+# vnf_securtiy_group - The security group to which the VSI interface belongs to.
+##############################################################################
+variable "vnf_security_group" {
+  default     = "ubuntu-security-group"
+  description = "The security group for VNF VPC"
+}
