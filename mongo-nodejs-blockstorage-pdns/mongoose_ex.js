3799a9081e8350bf124e745f1c8b5083053326c8
Malar Kandasamy <k.malarvizhi@in.ibm.com>
2020-09-09 09:20:48 +0530

    Added example for VSI with Block Storage

---
+++ mongo-nodejs-blockstorage-pdns/mongoose_ex.js
@@ -0,0 +1,24 @@
+
+const mongoose = require('mongoose')
+
+const options = {
+    autoIndex: false, // Don't build indexes
+    reconnectTries: 30, // Retry up to 30 times
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0
+  }
+
+const connectWithRetry = () => {
+  console.log('MongoDB connection with retry')
+  mongoose.connect("mongodb://dbuser:dbpassword@testa.testpdns.com:27017/user_db", options).then(()=>{
+    console.log('MongoDB is connected')
+
+  }).catch(err=>{
+    console.log('MongoDB connection unsuccessful, retry after 5 seconds.')
+    setTimeout(connectWithRetry, 5000)
+  })
+}
+
+connectWithRetry()
